dec64c26cd9a1e12fcabb050ed7f81cdffd8cb23
narellanoc <nicolas.arellano@kellogg.northwestern.edu>
2022-05-15 22:48:41 +0000

    2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+
+def new
+    @post = Place.new
+    @post["place_id"] = params["place_id"]
+end
+    
+def create
+    @post = Place.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
+    @post.save
+    redirect_to "/places"
+
 end
